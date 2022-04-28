c8d1122ad34b7c8b6b4a5bbff212fbd2320343a8
AlenaDoz <alena.abunassar1@mail.ru>
2022-04-28 16:26:25 +0300

    init: start basic js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,23 +1,32 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  let result = [];
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  members.forEach(function (member) {
+    if (typeof member === "string") {
+      result.push(member.split(" ").join("").toUpperCase()[0]);
+    }
+  });
+  result.sort();
+  return result.join("");
 }
 
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,66 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  let newArr = [];
+  let indArr = [];
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  arr.forEach(function (element, index) {
+    if (element === "--discard-next") {
+      indArr.push(element);
+      indArr.push(index);
+      if (arr.length - 1 >= index + 1) {
+        arr.splice(index, 2);
+      } else {
+        arr.splice(index, 1);
+      }
+    }
+    if (element === "--discard-prev") {
+      indArr.push(element);
+      indArr.push(index);
+      if (0 < index - 1) {
+        arr.splice(index - 1, 2);
+      } else {
+        arr.splice(index, 1);
+      }
+    }
+    if (element === "--double-next") {
+      indArr.push(element);
+      indArr.push(index);
+      if (arr.length - 1 >= index + 1) {
+        arr[index] = arr[index + 1];
+      } else {
+        arr.splice(index, 1);
+      }
+    }
+    if (element === "--double-prev") {
+      indArr.push(element);
+      indArr.push(index);
+      if (0 < index - 1) {
+        arr[index] = arr[index - 1];
+      } else {
+        arr.splice(index, 1);
+      }
+    }
+    newArr.push(element);
+  });
+  return arr;
 }
 
 module.exports = {
-  transform
+  transform,
 };
